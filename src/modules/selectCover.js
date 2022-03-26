c5425d11cab49380ec5d7fe156ba976485f6c2a4
etienne-bourganel <etienne.bourganel@gmail.com>
2022-03-26 14:37:27 +0100

    Add book 9

--- src/modules/selectCover.js
+++ src/modules/selectCover.js
@@ -1,34 +1,37 @@
-import orwell1984 from '../data/images/covers/1984.jpg'
-import twelve from '../data/images/covers/12_rules.png'
-import shantaram from '../data/images/covers/shantaram.jpg'
-import viSkuggorna from '../data/images/covers/vi-skuggorna.jpg'
-import foundation from '../data/images/covers/foundation.jpg'
-import factfulness from '../data/images/covers/factfulness.jpg'
-import theRoad from '../data/images/covers/theroad.jpg'
-import eufori from '../data/images/covers/eufori.jpg'
-import questionMark from '../data/images/covers/questionMark.png'
+import orwell1984 from '../data/images/covers/1984.jpg';
+import twelve from '../data/images/covers/12_rules.png';
+import shantaram from '../data/images/covers/shantaram.jpg';
+import viSkuggorna from '../data/images/covers/vi-skuggorna.jpg';
+import foundation from '../data/images/covers/foundation.jpg';
+import factfulness from '../data/images/covers/factfulness.jpg';
+import theRoad from '../data/images/covers/theroad.jpg';
+import eufori from '../data/images/covers/eufori.jpg';
+import homegoing from '../data/images/covers/homegoing.png';
+import questionMark from '../data/images/covers/questionMark.png';
 
 const selectCover = (bookID) => {
   switch (bookID) {
     case 1:
-      return orwell1984
+      return orwell1984;
     case 2:
-      return twelve
+      return twelve;
     case 3:
-      return shantaram
+      return shantaram;
     case 4:
-      return viSkuggorna
+      return viSkuggorna;
     case 5:
-      return foundation
+      return foundation;
     case 6:
-      return factfulness
+      return factfulness;
     case 7:
-      return theRoad
+      return theRoad;
     case 8:
-      return eufori
+      return eufori;
+    case 9:
+      return homegoing;
     default:
-      return questionMark
+      return questionMark;
   }
-}
+};
 
-export default selectCover
+export default selectCover;
